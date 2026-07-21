5f51046445f8b87b3fdc3cf532be75e9f5ec46b1
adnan6688 <adnanfarukgolam680@gmail.com>
2026-07-21 09:30:05 +0600

    updated env name

--- src/pages/api/allapi.ts
+++ src/pages/api/allapi.ts
@@ -5,7 +5,7 @@ import { getErrorMessage } from "../Error/getErrorMessage";
 
 export const userLogin = async (email: string, password: string) => {
     try {
-        const response = await axios.post(`${import.meta.env.VITE_BASLE_URL}/auth/login`, { email, password });
+        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/auth/login`, { email, password });
         const token = response?.data?.data?.accessToken;
         return {
             success: true,
